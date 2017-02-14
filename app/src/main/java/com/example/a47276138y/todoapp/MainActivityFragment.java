55c8f2837a8be3ef1c418972bd4c26948da4ba29
fcpau <fcpau4@gmail.com>
2017-02-14 19:16:10 +0100

    About to save images urls in Firebase.

--- app/src/main/java/com/example/a47276138y/todoapp/MainActivityFragment.java
+++ app/src/main/java/com/example/a47276138y/todoapp/MainActivityFragment.java
@@ -1,16 +1,19 @@
 package com.example.a47276138y.todoapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -18,12 +21,14 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import com.firebase.ui.auth.AuthUI;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
 import com.google.firebase.database.ValueEventListener;
+
+import static android.app.Activity.RESULT_OK;
+import static com.example.a47276138y.todoapp.Utils.REQUEST_TAKE_PHOTO;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -32,9 +37,7 @@ public class MainActivityFragment extends Fragment {
 
     private ArrayAdapter<String> adapter;
     private static final int RC_SIGN_IN = 123;
-    private TextView textView;
     private DatabaseReference myRef;
-    private EditText et_input;
 
 
 
@@ -49,36 +52,29 @@ public class MainActivityFragment extends Fragment {
 
         ArrayList<String> todos = new ArrayList<>();
 
-        ListView lvTodos = (ListView) view.findViewById(R.id.lv_todos);
-        Button btAdd = (Button) view.findViewById(R.id.button);
-        textView = (TextView) view.findViewById(R.id.textView);
-        et_input = (EditText) view.findViewById(R.id.et_input);
+
+        GridView gridView = (GridView) view.findViewById(R.id.gv_images);
+        Button btTakeImage = (Button) view.findViewById(R.id.bt_take_img);
+        Button recVideo = (Button) view.findViewById(R.id.bt_rec_video);
 
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("todos");
+        myRef = database.getReference("images");
 
-        setupAuth();
+        //setupAuth();
 
         adapter = new ArrayAdapter<String>(
                 getContext(),
-                R.layout.lv_todo_row,
-                R.id.textView,
+                R.layout.gv_image_square,
                 todos);
 
-        lvTodos.setAdapter(adapter);
-        btAdd.setOnClickListener(new View.OnClickListener() {
+        gridView.setAdapter(adapter);
+        btTakeImage.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+                dispatchTakePictureIntent();
 
-                String txt = et_input.getText().toString();
-                Log.d("XXXXXXXX", txt);
-
-                if(!txt.equals("")){
-                    DatabaseReference child = myRef.push();
-                    child.setValue(txt);
-                }
             }
         });
 
@@ -110,7 +106,48 @@ public class MainActivityFragment extends Fragment {
     }
 
 
-    private void setupAuth() {
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = Utils.createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.getMessage();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        switch(requestCode){
+            case 1:
+                if (resultCode == RESULT_OK) {
+
+                }
+
+                break;
+        }
+    }
+
+
+
+   /* private void setupAuth() {
 
         FirebaseAuth auth = FirebaseAuth.getInstance();
 
@@ -129,5 +166,5 @@ public class MainActivityFragment extends Fragment {
                             )
                             .build(),
                     RC_SIGN_IN);}
-    }
+    }*/
 }
---
+++ app/src/main/java/com/example/a47276138y/todoapp/Utils.java
@@ -0,0 +1,45 @@
+package com.example.a47276138y.todoapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static java.security.AccessController.getContext;
+
+/**
+ * Created by 47276138y on 14/02/17.
+ */
+
+public class Utils {
+
+    static String mCurrentPhotoPath;
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    public static File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+
+
+
+
+}
